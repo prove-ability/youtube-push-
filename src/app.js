45d33379bac8ad83e5ed04ba8d3fb25a0c479f57
prove-ability <prove.ability00@gmail.com>
2020-05-17 23:31:51 +0900

    텔레그램 봇 추가 및 메세지 추가

--- src/app.js
+++ src/app.js
@@ -1,61 +1,11 @@
-// "use strict";
-// const express = require("express");
-// const YouTubeNotifier = require("youtube-notification");
-
-// const app = express();
-// const notifier = new YouTubeNotifier({
-//   hubCallback: "https://18.221.54.230/youtube",
-//   secret: "Something",
-// });
-
-// // https://221.147.81.82/youtube
-
-// //https://www.youtube.com/xml/feeds/videos.xml?channel_id=CHANNEL_ID
-// notifier.setup();
-// app.use("/youtube", notifier.listener());
-
-// app.get("/", (req, res, next) => {
-//   res.send("hello world!");
-// });
-
-// app.listen(8080, () => console.log("Express server has started on port 8080"));
-
-// notifier.on("subscribe", (data) => {
-//   console.log("Subscribed");
-//   console.log(data);
-// });
-
-// notifier.on("unsubscribe", (data) => {
-//   console.log("Unsubscribed");
-//   console.log(data);
-// });
-
-// notifier.on("denied", (data) => {
-//   console.log("Denied");
-//   console.log(data);
-// });
-
-// notifier.on("notified", (data) => {
-//   console.log("New Video");
-//   console.log(data);
-// });
-
-// // notifier.subscribe([
-// //   "UCP8KMauNQ5YhTL8fzoSYcfQ", // 하나TV
-// //   "UC90Y-ClmElcidhXs2-x1EIg", // SK 증권리서치
-// //   "UCBM86JVoHLqg9irpR2XKvGw", // 달란트투자
-// //   "UChlv4GSd7OQl3js-jkLOnFA", // 삼프로 TV
-// //   "UCauZxKRDiGcrWplWUOIN9JQ", // 이리온 스튜디오
-// //   "UCsJ6RuBiTVWRX156FVbeaGg", // 슈카월드
-// //   "UC045CUnX2NJW6DNjSYezjTw", // TEST(Prove)
-// // ]);
-
-// notifier.subscribe("UC045CUnX2NJW6DNjSYezjTw");
-
+"use strict";
 const YouTubeNotifier = require("youtube-notification");
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "1110430648:AAGE5Mr8vF0-YFzzRlQgOiw1ikH3uaej_2s";
+const bot = new TelegramBot(token, { polling: true });
 
 const notifier = new YouTubeNotifier({
-  // hubCallback: "http://18.221.54.230",
   hubCallback: "http://18.221.54.230/youtube",
   port: 8080,
   secret: "Something",
@@ -66,6 +16,12 @@ notifier.setup();
 notifier.on("subscribe", (data) => {
   console.log("Subscribed");
   console.log(data);
+  // data
+  // { type: 'subscribe',
+  // channel: 'UChlv4GSd7OQl3js-jkLOnFA',
+  // lease_seconds: '432000' }
+  const chatId = "-1001225087031";
+  bot.sendMessage(chatId, "Received your message");
 });
 
 notifier.on("unsubscribe", (data) => {
